f2ac3ea15192c76ef50dff5ae968b7b5131605b8
Al Amin <hmalaminmb4@gmail.com>
2026-07-21 22:34:30 +0600

    Schema based meta data

--- assets/js/src/admin.asset.php
+++ assets/js/src/admin.asset.php
@@ -1,4 +1,1 @@
-<?php return array(
-	'dependencies' => array( 'react-jsx-runtime', 'wp-components', 'wp-core-data', 'wp-editor', 'wp-i18n', 'wp-plugins' ),
-	'version'      => '476611ebbcb3cba4a882',
-);
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-components', 'wp-core-data', 'wp-editor', 'wp-i18n', 'wp-plugins'), 'version' => '476611ebbcb3cba4a882');
--- src/CPT/PostMeta.php
+++ src/CPT/PostMeta.php
@@ -9,73 +9,59 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 class PostMeta implements Registerable {
-	private const META_PREIFX = 'awesome_bookly_';
+	private const META_PREFIX = 'awesome_bookly_';
 
 	#[Override]
 	public function register_hook() {
 		add_action( 'init', array( $this, 'register_post_meta' ) );
 	}
 
+	private function get_default_args() {
+		return array(
+			'single'            => true,
+			'show_in_rest'      => true,
+			'sanitize_callback' => 'sanitize_text_field',
+			'auth_callback'     => static fn() => current_user_can( 'edit_posts' ),
+		);
+	}
+
 	public function register_post_meta() {
+		$default_args = $this->get_default_args();
+
 		// Decalre all the meta keys we need.
 		$schema = array(
-			self::META_PREIFX . 'isbn'           => array(
-				'type'              => 'string',
-				'single'            => true,
-				'show_in_rest'      => true,
-				'sanitize_callback' => 'sanitize_text_field',
-				'auth_callabck'     => function () {
-					return current_user_can( 'edit_posts' );
-				},
+			self::META_PREFIX . 'isbn'           => array(
+				'type' => 'string',
+				...$default_args,
 			),
-			self::META_PREIFX . 'pub_date'       => array(
-				'type'              => 'string',
-				'single'            => true,
-				'show_in_rest'      => true,
-				'sanitize_callback' => 'sanitize_text_field',
-				'auth_callabck'     => function () {
-					return current_user_can( 'edit_posts' );
-				},
+			self::META_PREFIX . 'pub_date'       => array(
+				'type' => 'string',
+				...$default_args,
 			),
-			self::META_PREIFX . 'lang'           => array(
-				'type'              => 'string',
-				'single'            => true,
-				'show_in_rest'      => true,
-				'sanitize_callback' => 'sanitize_text_field',
-				'auth_callabck'     => function () {
-					return current_user_can( 'edit_posts' );
-				},
+			self::META_PREFIX . 'lang'           => array(
+				'type' => 'string',
+				...$default_args,
 			),
-			self::META_PREIFX . 'page_count'     => array(
-				'type'              => 'string',
-				'single'            => true,
-				'show_in_rest'      => true,
-				'sanitize_callback' => 'sanitize_text_field',
-				'auth_callabck'     => function () {
-					return current_user_can( 'edit_posts' );
-				},
+			self::META_PREFIX . 'page_count'     => array(
+				'type' => 'integer',
+				...$default_args,
 			),
-			self::META_PREIFX . 'price'          => array(
-				'type'              => 'string',
-				'single'            => true,
-				'show_in_rest'      => true,
-				'sanitize_callback' => 'sanitize_text_field',
-				'auth_callabck'     => function () {
-					return current_user_can( 'edit_posts' );
-				},
+			self::META_PREFIX . 'price'          => array(
+				'type' => 'integer',
+				...$default_args,
 			),
-			self::META_PREIFX . 'gallery_images' => array(
+			self::META_PREFIX . 'gallery_images' => array(
 				'type'          => 'array',
 				'single'        => true,
 				'show_in_rest'  => array(
 					'schema' => array(
-						'type'  => '',
+						'type'  => 'array',
 						'items' => array(
 							'type' => 'integer',
 						),
 					),
 				),
-				'auth_callabck' => function () {
+				'auth_callback' => function () {
 					return current_user_can( 'edit_posts' );
 				},
 			),
