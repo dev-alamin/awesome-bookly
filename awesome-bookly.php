cfbec3f8087dec849e5c52b0730e4ae1b3b37b17
Al Amin <hmalaminmb4@gmail.com>
2026-07-21 12:50:19 +0600

    Initial Commit

---
+++ awesome-bookly.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: Awesome Bookly
+ * Description: A plugin to help organizing books, using gutenberg as well.
+ * Version: 0.1.0
+ * Author: Al Amin
+ * Author URI: https://github.com/dev-alamin
+ * Plugin URI: https://github.com/dev-alamin/awesome-bookly
+ * @package AsmBookly
+ */
+
+if( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
